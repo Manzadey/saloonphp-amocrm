24b11ecf5f65a49cd71a0ff80d4e89fbb589fe49
manzadey <andrey.manzadey@gmail.com>
2026-01-22 20:52:03 +0300

    Обновить тест ConfigTest для устранения использования Mock
    Устранено использование Mock-объектов в тестах ConfigTest:
    
    - Изменено пространство имен теста на `Manzadey\SaloonAmoCrm\Tests\Configs`.
    - В методе `setUp` инициализирован экземпляр класса Config с тестовыми данными вместо Mock-объекта.
    - Удалены ожидания методов в тестах `testBaseDomain`, `testRedirectUri`, `testClientId` и `testClientSecret`.
    - Обновлены проверки в тестах на работу с реальным экземпляром класса.
    
    Эти изменения повышают надежность тестов за счет работы с реальной реализацией класса, а также улучшены читаемость и поддержка тестов.

--- tests/Configs/ConfigTest.php
+++ tests/Configs/ConfigTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Manzadey\SaloonAmoCrm\Tests;
+namespace Manzadey\SaloonAmoCrm\Tests\Configs;
 
 use Manzadey\SaloonAmoCrm\Configs\Config;
 use PHPUnit\Framework\TestCase;
@@ -13,38 +13,31 @@ class ConfigTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->config = $this->createMock(Config::class);
+        $this->config = new Config(
+            'domain.amocrm.ru',
+            'https://example.com/callback',
+            'client_id',
+            'client_secret'
+        );
     }
 
     public function testBaseDomain(): void
     {
-        $this->config->expects($this->once())
-            ->method('getBaseDomain')
-            ->willReturn('domain.amocrm.ru');
         $this->assertEquals('domain.amocrm.ru', $this->config->getBaseDomain());
     }
 
     public function testRedirectUri(): void
     {
-        $this->config->expects($this->once())
-            ->method('getRedirectUri')
-            ->willReturn('https://example.com/callback');
         $this->assertEquals('https://example.com/callback', $this->config->getRedirectUri());
     }
 
     public function testClientId(): void
     {
-        $this->config->expects($this->once())
-            ->method('getClientId')
-            ->willReturn('client_id');
         $this->assertEquals('client_id', $this->config->getClientId());
     }
 
     public function testClientSecret(): void
     {
-        $this->config->expects($this->once())
-            ->method('getClientSecret')
-            ->willReturn('client_secret');
         $this->assertEquals('client_secret', $this->config->getClientSecret());
     }
 }
