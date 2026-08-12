c083e3d2acc2df8a2e4fe931d61b4493e20fc094
Andrey Manzadey <andrey.manzadey@gmail.com>
2026-08-12 10:53:56 +0300

    feat: add pagination and typed response to UserListRequest
    Every other list request in the package supports page()/limit() and
    returns a typed Response; UserListRequest was the odd one out and
    returned the bare Saloon\Response. Bring it in line with
    ContactListRequest/LeadListRequest by adding HasPageQuery/HasLimitQuery
    and a new UserListResponse (users(), page(), links()).

--- src/Modules/User/Requests/UserListRequest.php
+++ src/Modules/User/Requests/UserListRequest.php
@@ -4,9 +4,27 @@ declare(strict_types=1);
 
 namespace Manzadey\SaloonAmoCrm\Modules\User\Requests;
 
+use Manzadey\SaloonAmoCrm\Modules\User\Responses\UserListResponse;
+use Manzadey\SaloonAmoCrm\Query;
+use Manzadey\SaloonAmoCrm\Requests\SendsTypedResponse;
 use Saloon\Enums\Method;
 
 class UserListRequest extends AbstractUserRequest
 {
+    use SendsTypedResponse;
+    use Query\HasPageQuery;
+    use Query\HasLimitQuery;
+
     protected Method $method = Method::GET;
+
+    protected ?string $response = UserListResponse::class;
+
+    /**
+     * @throws \Saloon\Exceptions\Request\FatalRequestException
+     * @throws \Saloon\Exceptions\Request\RequestException
+     */
+    public function send(): UserListResponse
+    {
+        return $this->sendTyped(UserListResponse::class);
+    }
 }
